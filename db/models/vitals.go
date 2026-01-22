42ed2e774ba1e06d86ee579bdb7397a434d9c60c
edfincham <ed.fincham@proton.me>
2026-01-22 08:42:16 +0100

    fix(serial): add device id to database

--- db/models/vitals.go
+++ db/models/vitals.go
@@ -11,43 +11,44 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func InsertVitals(db *db.PostgresPool, v sock.Vitals) error {
+func InsertVitals(db *db.PostgresPool, v sock.Vitals, serial string) error {
 	query := `
 		INSERT INTO Vitals (
 			ts, ox, hr, mv, sc, st, bso, bat, btt, chg, aps, alrt, ota, srf, rsi,
-			sb, ss, mvb, oxta, onm, bsb, mrs, hw
+			sb, ss, mvb, oxta, onm, bsb, mrs, hw, serial
 		) VALUES (
 			@ts, @ox, @hr, @mv, @sc, @st, @bso, @bat, @btt, @chg, @aps, @alrt, @ota, @srf, @rsi,
-			@sb, @ss, @mvb, @oxta, @onm, @bsb, @mrs, @hw
+			@sb, @ss, @mvb, @oxta, @onm, @bsb, @mrs, @hw, @serial
 		)`
 
 	now := time.Now()
 	timestamp := time.Unix(now.Unix(), 0)
 
 	args := pgx.NamedArgs{
-		"ts":   timestamp,
-		"ox":   v.Ox,
-		"hr":   v.Hr,
-		"mv":   v.Mv,
-		"sc":   v.Sc,
-		"st":   v.St,
-		"bso":  v.Bso,
-		"bat":  v.Bat,
-		"btt":  v.Btt,
-		"chg":  v.Chg,
-		"aps":  v.Aps,
-		"alrt": v.Alrt,
-		"ota":  v.Ota,
-		"srf":  v.Srf,
-		"rsi":  v.Rsi,
-		"sb":   v.Sb,
-		"ss":   v.Ss,
-		"mvb":  v.Mvb,
-		"oxta": v.Oxta,
-		"onm":  v.Onm,
-		"bsb":  v.Bsb,
-		"mrs":  v.Mrs,
-		"hw":   v.Hw,
+		"ts":     timestamp,
+		"ox":     v.Ox,
+		"hr":     v.Hr,
+		"mv":     v.Mv,
+		"sc":     v.Sc,
+		"st":     v.St,
+		"bso":    v.Bso,
+		"bat":    v.Bat,
+		"btt":    v.Btt,
+		"chg":    v.Chg,
+		"aps":    v.Aps,
+		"alrt":   v.Alrt,
+		"ota":    v.Ota,
+		"srf":    v.Srf,
+		"rsi":    v.Rsi,
+		"sb":     v.Sb,
+		"ss":     v.Ss,
+		"mvb":    v.Mvb,
+		"oxta":   v.Oxta,
+		"onm":    v.Onm,
+		"bsb":    v.Bsb,
+		"mrs":    v.Mrs,
+		"hw":     v.Hw,
+		"serial": serial,
 	}
 
 	_, err := db.DB.Exec(context.Background(), query, args)
